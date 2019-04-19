33f9b92aa6b97dd9be024532e56ade582da7da57
wangfeng <274021963@qq.com>
2019-04-19 16:04:25 +0800

    修改接口参数

--- app/src/main/java/com/th/guangxismz/Impl/GxSmzSdkListner.java
+++ app/src/main/java/com/th/guangxismz/Impl/GxSmzSdkListner.java
@@ -3,13 +3,14 @@ package com.th.guangxismz.Impl;
 import android.graphics.Bitmap;
 
 import com.th.guangxismz.Bean.Employee;
+import com.th.guangxismz.Bean.EmployeeListBean;
 
 import java.util.List;
 
 public interface GxSmzSdkListner {
 
     void deleteBaiduFace(String empId);
-    void faceRegister(Bitmap bitmap,String empId,String username);
+    void faceRegister(Bitmap bitmap, EmployeeListBean bean);
     void cleanAllFace(List<Employee> employees);
     void loadFacesFromDB();
     void projectCount(int pNumber);
